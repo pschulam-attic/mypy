50a7c94f053ff7e83f61873846155f4510e9274b
Peter Schulam <pschulam@gmail.com>
2015-07-23 12:32:29 -0400

    Add check_grad function.

--- mypy/util.py
+++ mypy/util.py
@@ -15,3 +15,16 @@ def days_between(now, then):
     except AttributeError:
         return np.nan
 
+
+def check_grad(f, x0, eps=1e-8):
+    f0 = f(x0)
+    n = x0.size
+    g = np.zeros_like(x0)
+    for i in range(n):
+        dt = np.zeros_like(x0)
+        dt[i] += eps
+        f1 = f(x0 + dt)
+        g[i] = (f1 - f0) / eps
+        
+    return g
+
